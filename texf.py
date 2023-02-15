6f5ee184f4585b28de8ea017800205d175a3f2fa
SlashCreate <112573796+SlashCreate@users.noreply.github.com>
2023-02-15 15:10:15 -0500

    texf

--- texf.py
+++ texf.py
@@ -1,10 +1,23 @@
+global var
+var = {'null': 'null'}
+
 def run(cmd: str, spliter=' *', errmsg=False):
   arg = cmd.split(spliter)
-  if arg[0] == 'pt':
-    print(arg[1])
-  else:
+  try:
+    if arg[0] == 'pt':
+      print(arg[1])
+    if arg[0] == 'pv':
+      print(var[arg[1]])
+    elif arg[0] == 'vs':
+      var[str(arg[1])] = str(arg[2])
+    else:
+      if errmsg:
+        print('command error')
+      else:
+        pass
+  except:
     if errmsg:
-      print('error')
+      print('syntax error')
     else:
       pass
 
